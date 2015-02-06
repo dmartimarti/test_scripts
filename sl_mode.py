7f75b76115ac748eea18bfe42d4ed27caf7d975d
Dani <dmartimarti@gmail.com>
2015-02-06 15:33:06 +0100

    now iterates over all sheets in one xlsx file

--- sl_mode.py
+++ sl_mode.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# module load gcc/gcc-4.8.0
+# module load python/2.7.5
 
 import pandas as pd 
-import xlrd
 from pandas import ExcelWriter
 
 # py_slidingWindow_xlsx release information
@@ -19,7 +20,7 @@ if(_devflag):
     print('\n>>> WARNING! THIS IS JUST A DEVELOPMENT SUBRELEASE.' + 
           ' USE IT AT YOUR OWN RISK!')  
 
-file_name = "MPHM.xlsx"
+file_name = "IBS.xlsx"
 
 xl_file = pd.ExcelFile(file_name)
 
@@ -28,31 +29,29 @@ dfs = {sheet_name: xl_file.parse(sheet_name)
 
 # slow slmode version (as R script)
 
-def slmode(size, output):
-	writer = ExcelWriter(output)
-	columnas = dfs["h_male_feces"].columns # store columns names
-	length = len(dfs["h_male_feces"].columns)
-	new_df = pd.DataFrame(dfs["h_male_feces"].iloc[:,0])
+def slmode(sheet, size):
+	writer = ExcelWriter("sw_mode_" + str(size) + "t_" + sheet + ".xlsx")
+	columnas = dfs[str(sheet)].columns # store columns names
+	length = len(dfs[str(sheet)].columns)
+	new_df = pd.DataFrame(dfs[str(sheet)].iloc[:,0])
 	for i in range(1,length-(size-1)):
 		for j in range(0,(size)):
-			new_df[str(columnas[j+i])] = dfs["h_male_feces"].iloc[:,j+i]
-		new_df.to_excel(writer,"set " + str(i), index=False)
-		new_df = pd.DataFrame(dfs["h_male_feces"].iloc[:,0])
+			new_df[str(columnas[j+i])] = dfs[str(sheet)].iloc[:,j+i]
+		new_df.to_excel(writer,str(sheet) + "set " + str(i), index=False)
+		new_df = pd.DataFrame(dfs[str(sheet)].iloc[:,0])
 	writer.save()
 
+def sw_authomatic_mode():
+	sheets = xl_file.sheet_names
+	for name in sheets:
+		slmode(name, 5)
 
-slmode(4, "male_feces_test.xlsx")
+sw_authomatic_mode()
 
 
 
 ###############
 ##  TO DO:
-##		- see if I can extract the name of each column. That should be in the reading_sheet loop above, I have to see
-##		  which options I have to modify
-##  	- create a loop to iterate over each column. Slow slmode as the R script
-## 		-
-
-
-
+##		
 
 
